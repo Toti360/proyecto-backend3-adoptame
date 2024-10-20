6f9202005e05a3448ab1c564c31e83ebf78b982f
Roxana Abugauch <aburoxana@hotmail.com>
2024-10-20 01:01:07 -0300

    Primer Preentrega

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+mongoose.set('strictQuery', false);
 import cookieParser from 'cookie-parser';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import mocksRouter from "./routes/mocks.router.js";
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(`mongodb+srv://aburoxana:917325@cluster0.wvf05f6.mongodb.net/backend3?retryWrites=true&w=majority&appName=Cluster0`)
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,5 +20,6 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use("/api/mocks", mocksRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
---
+++ src/controllers/mocks.controller.js
@@ -0,0 +1,43 @@
+import { petsService, usersService } from "../services/index.js";
+import MockingService from "../services/mocking.js";
+
+const getMockingPets = async (req, res) => {
+    const pets = await MockingService.generateMockingPets(100); 
+    res.send({status: "success", payload: pets}); 
+}
+
+const getMockingUsers = async (req, res) => {
+    const users = await MockingService.generateMockingUsers(50);
+    res.send({status: "success", payload: users}); 
+}
+
+const generateData = async (req, res) => {
+    const { users, pets } = req.body; 
+    try {
+        //Generación de usuarios falsos: 
+        const mockingUsers = await MockingService.generateMockingUsers(users);
+
+        //Generación de mascotas falsas: 
+        const mockingPets = await MockingService.generateMockingPets(pets);
+
+        //Insertar los datos generados en la BD: 
+        await Promise.all(
+            mockingUsers.map(user => usersService.create(user)),
+            mockingPets.map(pet => petsService.create(pet))
+        );
+        
+        res.send({
+            status: "success",
+            message: "Los datos se han generado y cargado correctamente en la Base de Datos"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error al generar y cargar los datos.!!!"); 
+    }
+}
+
+export default {
+    getMockingPets,
+    getMockingUsers,
+    generateData
+}
